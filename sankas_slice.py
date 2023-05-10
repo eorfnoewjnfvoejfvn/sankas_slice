c4618297297b931abde7dfab659a7cb3f0e4c83e
Thisanka <19tweeraddana@wakatipu.school.nz>
2023-05-10 21:31:11 +1200

    added working input validation to the form gui

--- sankas_slice.py
+++ sankas_slice.py
@@ -1,14 +1,9 @@
 import tkinter as tk
-from tkinter import messagebox
-from tkinter import ttk
-import re
-
-
-import tkinter as tk
-from tkinter import ttk
+from tkinter import messagebox, ttk
 
 class FormGUI:
-    def __init__(self):
+    def __init__(self, order_summary):
+        self.order_summary = order_summary
         self.window = tk.Tk()
         self.window.title("Form Window")
 
@@ -36,9 +31,77 @@ class FormGUI:
         self.entry_phone_number = ttk.Entry(self.window, textvariable=self.phone_number_var)
         self.entry_phone_number.grid(row=2, column=1, padx=10, pady=10)
 
-        # Add a submit button
+        # Validate fields on entry
+        self.entry_first_name.bind("<KeyRelease>", self.validate_first_name)
+        self.entry_last_name.bind("<KeyRelease>", self.validate_last_name)
+        self.entry_phone_number.bind("<KeyRelease>", self.validate_phone_number)
+
+        # Create the submit button
         self.submit_button = ttk.Button(self.window, text="Submit", command=self.submit_form)
         self.submit_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+
+        self.submit_button.config(state="disabled")
+
+    def validate_first_name(self, event):
+            first_name = self.first_name_var.get()
+            if not re.match(r"^[A-Za-z]*$", first_name):
+                self.entry_first_name.config(style="Error.TEntry")
+                self.submit_button.config(state="disabled")
+            else:
+                self.entry_first_name.config(style="TEntry")
+                self.check_fields_completed()
+
+    def validate_last_name(self, event):
+            last_name = self.last_name_var.get()
+            if not re.match(r"^[A-Za-z]*$", last_name):
+                self.entry_last_name.config(style="Error.TEntry")
+                self.submit_button.config(state="disabled")
+            else:
+                self.entry_last_name.config(style="TEntry")
+                self.check_fields_completed()
+
+    def validate_phone_number(self, event):
+            phone_number = self.phone_number_var.get()
+            if not re.match(r"^\d{10}$", phone_number):
+                self.entry_phone_number.config(style="Error.TEntry")
+                self.submit_button.config(state="disabled")
+            else:
+                self.entry_phone_number.config(style="TEntry")
+                self.check_fields_completed()
+
+    def check_fields_completed(self):
+            first_name = self.first_name_var.get()
+            last_name = self.last_name_var.get()
+            phone_number = self.phone_number_var.get()
+
+            if first_name and last_name and phone_number:
+                self.submit_button.config(state="normal")
+            else:
+                self.submit_button.config(state="disabled")
+
+    def submit_form(self):
+        first_name = self.first_name_var.get()
+        last_name = self.last_name_var.get()
+        phone_number = self.phone_number_var.get()
+
+        if not first_name or not last_name or not phone_number:
+            messagebox.showerror("Error", "Please fill in all fields.")
+            return
+
+        if not re.match(r"^[A-Za-z]*$", first_name):
+            messagebox.showerror("Error", "Invalid first name.")
+            return
+
+        if not re.match(r"^[A-Za-z]*$", last_name):
+            messagebox.showerror("Error", "Invalid last name.")
+            return
+
+        if not re.match(r"^\d{10}$", phone_number):
+            messagebox.showerror("Error", "Invalid phone number.")
+            return
+
+        messagebox.showinfo("Success", " Detail's Colletected successfully.")
+        self.window.destroy()
 
 # Create an instance of the FormGUI class
 form = FormGUI()
